4b038b80916d99d20c2eeaf6d4f0776bb001da1c
anh0001 <anhrisn@gmail.com>
2024-11-23 01:26:08 +0900

    Fix error of odometry calculator

--- src/rt1_hardware/src/odometry_calculator.cpp
+++ src/rt1_hardware/src/odometry_calculator.cpp
@@ -165,3 +165,12 @@ void OdometryCalculator::timerCallback()
     updateOdometry();
     publishOdometry();
 }
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<OdometryCalculator>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
